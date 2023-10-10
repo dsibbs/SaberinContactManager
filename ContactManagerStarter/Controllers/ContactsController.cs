7b16b3f7b03d8f009764949bc4fd8af5fdd8e927
Dsibbs <dsibbliesjr@gmail.com>
2023-10-10 18:00:03 -0400

    Added error handling to remining actions

--- ContactManagerStarter/Controllers/ContactsController.cs
+++ ContactManagerStarter/Controllers/ContactsController.cs
@@ -49,46 +49,58 @@ namespace ContactManager.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "Error has occur in DeleteContact - " + ex.Message);
+                return StatusCode(500, "Error has occured in DeleteContact - " + ex.Message);
             }
         }
 
         public async Task<IActionResult> EditContact(Guid id)
         {
-            var contact = await _context.Contacts
-                .Include(x => x.EmailAddresses)
-                .Include(x => x.Addresses)
-                .FirstOrDefaultAsync(x => x.Id == id);
-
-            if (contact == null)
+            try
             {
-                return NotFound();
+                var contact = await _context.Contacts
+                    .Include(x => x.EmailAddresses)
+                    .Include(x => x.Addresses)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+
+                var viewModel = new EditContactViewModel
+                {
+                    Id = contact.Id,
+                    Title = contact.Title,
+                    FirstName = contact.FirstName,
+                    LastName = contact.LastName,
+                    DOB = contact.DOB,
+                    PrimaryEmail = contact.PrimaryEmail,
+                    EmailAddresses = contact.EmailAddresses,
+                    Addresses = contact.Addresses
+                };
+
+                return PartialView("_EditContact", viewModel);
             }
-
-            var viewModel = new EditContactViewModel
+            catch (Exception ex)
             {
-                Id = contact.Id,
-                Title = contact.Title,
-                FirstName = contact.FirstName,
-                LastName = contact.LastName,
-                DOB = contact.DOB,
-                PrimaryEmail = contact.PrimaryEmail,
-                EmailAddresses = contact.EmailAddresses,
-                Addresses = contact.Addresses
-            };
-
-            return PartialView("_EditContact", viewModel);
+                return StatusCode(400, "Error has occured in EditContact - " + ex.Message);
+            }
         }
-
         public async Task<IActionResult> GetContacts()
         {
-            var contactList = await _context.Contacts
-                .OrderBy(x => x.FirstName)
-                .ToListAsync();
+            try
+            {
+                var contactList = await _context.Contacts
+                    .OrderBy(x => x.FirstName)
+                    .ToListAsync();
 
-            return PartialView("_ContactTable", new ContactViewModel { Contacts = contactList });
+                return PartialView("_ContactTable", new ContactViewModel { Contacts = contactList });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error has occured in GetContacts - " + ex.Message);
+            }
         }
-
         public IActionResult Index()
         {
             return View();
@@ -102,72 +114,79 @@ namespace ContactManager.Controllers
         [HttpPost]
         public async Task<IActionResult> SaveContact([FromBody] SaveContactViewModel model)
         {
-            var contact = model.ContactId == Guid.Empty
-                ? new Contact { Title = model.Title, FirstName = model.FirstName, LastName = model.LastName, DOB = model.DOB, PrimaryEmail = model.PrimaryEmail }
-                : await _context.Contacts.Include(x => x.EmailAddresses).Include(x => x.Addresses).FirstOrDefaultAsync(x => x.Id == model.ContactId);
-
-            if (contact == null)
+            try
             {
-                return NotFound();
-            }
+                var contact = model.ContactId == Guid.Empty
+                    ? new Contact { Title = model.Title, FirstName = model.FirstName, LastName = model.LastName, DOB = model.DOB, PrimaryEmail = model.PrimaryEmail }
+                    : await _context.Contacts.Include(x => x.EmailAddresses).Include(x => x.Addresses).FirstOrDefaultAsync(x => x.Id == model.ContactId);
 
-            _context.EmailAddresses.RemoveRange(contact.EmailAddresses);
-            _context.Addresses.RemoveRange(contact.Addresses);
-
-            var found = true;
-            foreach (var email in model.Emails)
-            {
-                contact.EmailAddresses.Add(new EmailAddress
+                if (contact == null)
                 {
-                    Type = email.Type,
-                    Email = email.Email,
-                    Contact = contact
-                });
-                if (found)
-                {
-                    model.PrimaryEmail = email.Email;
-                    found = false;
+                    return NotFound();
                 }
-                if (email.Type == EmailType.Primary)
+
+                _context.EmailAddresses.RemoveRange(contact.EmailAddresses);
+                _context.Addresses.RemoveRange(contact.Addresses);
+
+                var found = true;
+                foreach (var email in model.Emails)
                 {
-                    model.PrimaryEmail = email.Email;
+                    contact.EmailAddresses.Add(new EmailAddress
+                    {
+                        Type = email.Type,
+                        Email = email.Email,
+                        Contact = contact
+                    });
+                    if (found)
+                    {
+                        model.PrimaryEmail = email.Email;
+                        found = false;
+                    }
+                    if (email.Type == EmailType.Primary)
+                    {
+                        model.PrimaryEmail = email.Email;
+                    }
                 }
-            }
 
 
-            foreach (var address in model.Addresses)
-            {
-                contact.Addresses.Add(new Address
+                foreach (var address in model.Addresses)
                 {
-                    Street1 = address.Street1,
-                    Street2 = address.Street2,
-                    City = address.City,
-                    State = address.State,
-                    Zip = address.Zip,
-                    Type = address.Type
-                });
+                    contact.Addresses.Add(new Address
+                    {
+                        Street1 = address.Street1,
+                        Street2 = address.Street2,
+                        City = address.City,
+                        State = address.State,
+                        Zip = address.Zip,
+                        Type = address.Type
+                    });
+                }
+
+                contact.Title = model.Title;
+                contact.FirstName = model.FirstName;
+                contact.LastName = model.LastName;
+                contact.DOB = model.DOB;
+                contact.PrimaryEmail = model.PrimaryEmail;
+
+                if (model.ContactId == Guid.Empty)
+                {
+                    await _context.Contacts.AddAsync(contact);
+                }
+                else
+                {
+                    _context.Contacts.Update(contact);
+                }
+
+
+                await _context.SaveChangesAsync();
+                await _hubContext.Clients.All.SendAsync("Update");
+
+                return Ok();
             }
-
-            contact.Title = model.Title;
-            contact.FirstName = model.FirstName;
-            contact.LastName = model.LastName;
-            contact.DOB = model.DOB;
-            contact.PrimaryEmail = model.PrimaryEmail;
-
-            if (model.ContactId == Guid.Empty)
+            catch (Exception ex)
             {
-                await _context.Contacts.AddAsync(contact);
+                return StatusCode(400, "Error has occured in SaveContact - " + ex.Message);
             }
-            else
-            {
-                _context.Contacts.Update(contact);
-            }
-
-
-            await _context.SaveChangesAsync();
-            await _hubContext.Clients.All.SendAsync("Update");
-
-            return Ok();
         }
     }
 
